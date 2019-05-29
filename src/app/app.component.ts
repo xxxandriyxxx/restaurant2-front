d47f7592927a200c58b3d648cf447551121326bf
Andriy <asoha474@gmail.com>
2019-05-29 20:20:58 +0300

    add main-routing module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'restaurant2-front';
+
+
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  login() {
+    this.http.post('http://localhost:8080/login',
+      JSON.stringify({username: 'admin', password: 'admin'}),
+      {observe: 'response'}).subscribe(value => {
+      const token = value.headers.get('Authorization');
+      console.log(token);
+      console.log(value);
+
+      localStorage.setItem('_token', token);
+    });
+  }
+
+  getInfo() {
+    const headersOption = new HttpHeaders({'Authorization': localStorage.getItem('_token')});
+    // 'Authorization' в Сергія з лапками, хоче без лапок і працює теж
+    this.http.get('http://localhost:8080/get', {headers: headersOption, responseType: 'text'}).subscribe(value => console.log(value));
+    // this.http.get('http://localhost:8080/get', {responseType: 'text'}).subscribe(value => console.log(value));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {SignupModule} from './signup/signup.module';
+import {SigninModule} from './signin/signin.module';
+import {MainRoutingModule} from './main-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MainRoutingModule,
+    SignupModule,
+    SigninModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
