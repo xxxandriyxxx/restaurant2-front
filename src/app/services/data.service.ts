a9a3bcd182f8ad97d845f439006d72abbce58a43
Andriy <asoha474@gmail.com>
2019-08-10 16:43:33 +0300

    Changed data.service.ts: changed path to local resources. Deleted logos from static resources.

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,17 +6,18 @@ import {HttpHeaders} from '@angular/common/http';
 })
 export class DataService {
 
+  // URL for of the API
+  public url = 'http://localhost:8080';
+  // public url = 'http://ec2-18-219-114-136.us-east-2.compute.amazonaws.com:8080';
+
+  // this resource location is set by the resource handler in the WebConfig class in the API project
+  public pathToResources = this.url + '/logo';
+
   public passLoginRegExp = new RegExp('^[a-zA-Z0-9]{3,20}$');
   public emailRegExp = new RegExp('^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$');
   public priceRegExp = new RegExp('^\\d+(\\.\\d{1,2})?$');
   public siteRegExp = new RegExp('[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)');
   public phoneRegExp = new RegExp('^\\+\\d{2}\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}$');
-
-  public pathToResources = '../../../../assets/img';
-  // public pathToResources = 'logo/'; // used to building angular project and deploy on AWS
-
-  public url = 'http://localhost:8080';
-  // public url = 'http://ec2-3-19-69-114.us-east-2.compute.amazonaws.com:8080'; // used to building angular project and deploy on AWS
 
 
   constructor() {
